7ab37a53ba9d884381bfb6e64235698e9f045a9c
adel <72595848+adeliapramitha@users.noreply.github.com>
2021-12-19 21:33:22 +0700

    Add files via upload

--- dataprodi.sql
+++ dataprodi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Des 2021 pada 16.36
+-- Waktu pembuatan: 19 Des 2021 pada 15.35
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -50,7 +50,33 @@ INSERT INTO `buku` (`id_buku`, `nama_buku`, `pengarang`, `deskripsi`) VALUES
 (20, 'THE ENCHANTRESS RETURNS (THE LAND OF STORIES #2)', 'CHRIS COLFER', 'Synopsis:\r\n\r\nAfter decades of hiding, the evil Enchantress who cursed Sleeping Beauty is back with a'),
 (21, 'THE WISHING SPELL (THE LAND OF STORIES #1)', 'CHRIS COLFER', 'Synopsis:\r\n\r\nAlex and Conner Bailey’s world is about to change, in this fast-paced adventure that un'),
 (22, 'SEA WITCH', 'SARAH HENNING', 'Synopsis:\r\n\r\nEveryone knows what happens in the end. A mermaid, a prince, a true love’s kiss. But be'),
-(23, 'Thinking, Fast and Slow', 'Daniel Kahneman', 'Berpikir, Cepat dan Lambat adalah buku 2011 oleh psikolog Israel Daniel Kahneman. Tesis utama buku i');
+(23, 'Thinking, Fast and Slow', 'Daniel Kahneman', 'Berpikir, Cepat dan Lambat adalah buku 2011 oleh psikolog Israel Daniel Kahneman. Tesis utama buku i'),
+(24, 'Harry Potter', 'J.K ROWLING', 'abcdefg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_peminjaman_buku`
+--
+
+CREATE TABLE `detail_peminjaman_buku` (
+  `id_detail_peminjaman_buku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `detail_peminjaman_buku`
+--
+
+INSERT INTO `detail_peminjaman_buku` (`id_detail_peminjaman_buku`, `id_peminjaman_buku`, `id_buku`, `qty`) VALUES
+(5, 1, 10, 1),
+(6, 2, 10, 1),
+(7, 3, 23, 1),
+(8, 4, 18, 1),
+(9, 5, 15, 2),
+(10, 6, 24, 3);
 
 -- --------------------------------------------------------
 
@@ -111,6 +137,54 @@ INSERT INTO `mahasiswa` (`id_mhs`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `al
 (18, 'Bagus Mughi P', '2000-06-11', 'L', 'Desa Gedangsewu, Pare', 'bagusmughi', 'mumughi', 18),
 (21, 'Kim Seon Ho', '1986-01-28', 'L', 'Busan', 'hongbanjang', '123', 17);
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman_buku`
+--
+
+CREATE TABLE `peminjaman_buku` (
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `id_mhs` int(11) NOT NULL,
+  `tanggal_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `peminjaman_buku`
+--
+
+INSERT INTO `peminjaman_buku` (`id_peminjaman_buku`, `tanggal_pinjam`, `id_mhs`, `tanggal_kembali`) VALUES
+(1, '2021-12-06', 1, '2021-12-11'),
+(2, '2021-12-06', 1, '2021-12-11'),
+(3, '2021-12-06', 1, '2021-12-11'),
+(4, '2021-12-13', 1, '2021-12-18'),
+(5, '2021-12-13', 1, '2021-12-18'),
+(6, '2021-12-17', 1, '2021-12-22');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian_buku`
+--
+
+CREATE TABLE `pengembalian_buku` (
+  `id_pengembalianbuku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `denda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengembalian_buku`
+--
+
+INSERT INTO `pengembalian_buku` (`id_pengembalianbuku`, `id_peminjaman_buku`, `tanggal_pengembalian`, `denda`) VALUES
+(1, 2, '2021-12-06', 0),
+(2, 3, '2021-12-13', 10000),
+(3, 1, '2021-12-13', 10000),
+(4, 4, '2021-12-13', 0);
+
 --
 -- Indexes for dumped tables
 --
@@ -120,6 +194,14 @@ INSERT INTO `mahasiswa` (`id_mhs`, `nama`, `tanggal_lahir`, `jenis_kelamin`, `al
 --
 ALTER TABLE `buku`
   ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD PRIMARY KEY (`id_detail_peminjaman_buku`),
+  ADD KEY `id_peminjaman_buku` (`id_peminjaman_buku`),
+  ADD KEY `id_buku` (`id_buku`);
 
 --
 -- Indeks untuk tabel `jurusan`
@@ -135,6 +217,20 @@ ALTER TABLE `mahasiswa`
   ADD KEY `id_jurusan` (`id_jurusan`);
 
 --
+-- Indeks untuk tabel `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD PRIMARY KEY (`id_peminjaman_buku`),
+  ADD KEY `id_mhs` (`id_mhs`);
+
+--
+-- Indeks untuk tabel `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD PRIMARY KEY (`id_pengembalianbuku`),
+  ADD KEY `id_peminjaman_buku` (`id_peminjaman_buku`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -142,7 +238,13 @@ ALTER TABLE `mahasiswa`
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_buku` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  MODIFY `id_detail_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `jurusan`
@@ -157,14 +259,45 @@ ALTER TABLE `mahasiswa`
   MODIFY `id_mhs` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
+-- AUTO_INCREMENT untuk tabel `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  MODIFY `id_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  MODIFY `id_pengembalianbuku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
+
+--
+-- Ketidakleluasaan untuk tabel `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD CONSTRAINT `detail_peminjaman_buku_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_peminjaman_buku_ibfk_2` FOREIGN KEY (`id_peminjaman_buku`) REFERENCES `peminjaman_buku` (`id_peminjaman_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_jurusan`) REFERENCES `jurusan` (`id_jurusan`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD CONSTRAINT `peminjaman_buku_ibfk_1` FOREIGN KEY (`id_mhs`) REFERENCES `mahasiswa` (`id_mhs`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD CONSTRAINT `pengembalian_buku_ibfk_1` FOREIGN KEY (`id_peminjaman_buku`) REFERENCES `peminjaman_buku` (`id_peminjaman_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
